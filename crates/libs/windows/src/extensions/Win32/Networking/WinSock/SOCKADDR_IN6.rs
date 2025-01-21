7e345e060f02f8c44279fd4b5606a3222eebbfe6
Guilhem Vallat <guilhem.vallat@gmail.com>
2025-01-21 11:58:53 +0100

    Fix Networking no-std support

--- crates/libs/windows/src/extensions/Win32/Networking/WinSock/SOCKADDR_IN6.rs
+++ crates/libs/windows/src/extensions/Win32/Networking/WinSock/SOCKADDR_IN6.rs
@@ -1,7 +1,7 @@
 use crate::Win32::Networking::WinSock::{AF_INET6, SOCKADDR_IN6, SOCKADDR_IN6_0};
 
-impl From<std::net::SocketAddrV6> for SOCKADDR_IN6 {
-    fn from(addr: std::net::SocketAddrV6) -> Self {
+impl From<core::net::SocketAddrV6> for SOCKADDR_IN6 {
+    fn from(addr: core::net::SocketAddrV6) -> Self {
         // addr.port() and addr.flowinfo() are in host byte order
         // sin6_port and sin6_flowinfo must be big-endian, network byte order
         // sin6_scope_id is a bitfield without endianness
