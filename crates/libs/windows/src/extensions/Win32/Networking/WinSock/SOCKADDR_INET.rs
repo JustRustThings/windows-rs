c37d1b06880050db26f26edbc7ec8dde9e79fe7c
Guilhem Vallat <guilhem.vallat@gmail.com>
2025-01-21 11:58:53 +0100

    Fix Networking no-std support

--- crates/libs/windows/src/extensions/Win32/Networking/WinSock/SOCKADDR_INET.rs
+++ crates/libs/windows/src/extensions/Win32/Networking/WinSock/SOCKADDR_INET.rs
@@ -1,20 +1,20 @@
 use crate::Win32::Networking::WinSock::SOCKADDR_INET;
 
-impl From<std::net::SocketAddrV4> for SOCKADDR_INET {
-    fn from(addr: std::net::SocketAddrV4) -> Self {
+impl From<core::net::SocketAddrV4> for SOCKADDR_INET {
+    fn from(addr: core::net::SocketAddrV4) -> Self {
         SOCKADDR_INET { Ipv4: addr.into() }
     }
 }
-impl From<std::net::SocketAddrV6> for SOCKADDR_INET {
-    fn from(addr: std::net::SocketAddrV6) -> Self {
+impl From<core::net::SocketAddrV6> for SOCKADDR_INET {
+    fn from(addr: core::net::SocketAddrV6) -> Self {
         SOCKADDR_INET { Ipv6: addr.into() }
     }
 }
-impl From<std::net::SocketAddr> for SOCKADDR_INET {
-    fn from(addr: std::net::SocketAddr) -> Self {
+impl From<core::net::SocketAddr> for SOCKADDR_INET {
+    fn from(addr: core::net::SocketAddr) -> Self {
         match addr {
-            std::net::SocketAddr::V4(socket_addr_v4) => socket_addr_v4.into(),
-            std::net::SocketAddr::V6(socket_addr_v6) => socket_addr_v6.into(),
+            core::net::SocketAddr::V4(socket_addr_v4) => socket_addr_v4.into(),
+            core::net::SocketAddr::V6(socket_addr_v6) => socket_addr_v6.into(),
         }
     }
 }
