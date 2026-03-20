ac29e7c166c762e6bc6c5aa1fee3041c99a48c3e
Guilhem Vallat <guilhem.vallat@gmail.com>
2026-03-20 16:01:15 +0100

    Restore support for win7 again

--- crates/libs/bindgen/src/tables/method_def.rs
+++ crates/libs/bindgen/src/tables/method_def.rs
@@ -41,21 +41,9 @@ impl MethodDef {
     }
 
     pub fn module_name(&self) -> String {
-        const combase_functions: [&str; 5] = [
-            "CoCreateFreeThreadedMarshaler",
-            "CoIncrementMTAUsage",
-            "CoTaskMemAlloc",
-            "CoTaskMemFree",
-            "RoGetAgileReference",
-        ];
-
-        if combase_functions.contains(&self.name()) {
-            "combase.dll".to_string()
-        } else {
-            self.impl_map()
-                .map_or("", |map| map.scope().name())
-                .to_lowercase()
-        }
+        self.impl_map()
+            .map_or("", |map| map.scope().name())
+            .to_lowercase()
     }
 
     pub fn calling_convention(&self) -> &'static str {
